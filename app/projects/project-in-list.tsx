2d4b636134de86f90f8fd7047a96c5ef230f51eb
magoz <apps@magoz.is>
2024-05-19 19:11:21 +0200

    project actions

--- app/projects/project-in-list.tsx
+++ app/projects/project-in-list.tsx
@@ -1,24 +1,80 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { useAtom } from 'jotai'
 import { projectsAtom, focusPeriodsAtom } from '@/lib/local-state'
-import { Edit3Icon, TargetIcon, ArchiveIcon, ArchiveRestoreIcon, Trash2Icon } from 'lucide-react'
+import { Edit3Icon, ArchiveIcon, ArchiveRestoreIcon, Trash2Icon, EllipsisIcon } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { Project } from '@/lib/types'
-import { EditProject } from './edit'
+import { Input } from '@/components/ui/input'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { cn } from '@/lib/utils'
+import ColorPicker, { themes } from 'react-pick-color'
+import { Popover, PopoverContent } from '@/components/ui/popover'
+import { PopoverAnchor } from '@radix-ui/react-popover'
+import { colors } from '@/lib/defaults'
 
 type Props = {
   project: Project
 }
 
 export const ProjectInList = ({ project }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null)
   const [, setProjects] = useAtom(projectsAtom)
   const [, setFocusPeriods] = useAtom(focusPeriodsAtom)
-  const [editing, setEditing] = useState(false)
-  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false)
-  const { id, color, name } = project
+  const [isRenaming, setIsRenaming] = useState(false)
+  const [isRecoloring, setIsRecoloring] = useState(false)
+  const [showMenu, setShowMenu] = useState(false)
 
-  const toggleEdit = () => setEditing(prev => !prev)
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false)
+  const { id, color, name, isArchived, isDeleted } = project
+
+  const toggleIsRenaming = (value?: boolean) => {
+    console.log('toggle!')
+    setShowMenu(false)
+    setIsRenaming(prev => (value !== undefined ? value : !prev))
+    setTimeout(() => {
+      if (inputRef.current) {
+        inputRef.current.focus()
+        inputRef.current.select()
+      }
+    }, 150) // wait for the dropdownmenu to handle focus. Not ideal. Should be a better way
+    console.log(document.activeElement)
+  }
+
+  const openRecoloring = () => {
+    setShowMenu(false)
+    setTimeout(() => {
+      console.log(document.activeElement)
+      setIsRecoloring(true)
+    }, 200) // wait for the dropdownmenu to handle focus. Not ideal. Should be a better way
+    console.log(document.activeElement)
+  }
+
+  // useEffect(() => {
+  //   if (isRenaming) inputRef.current?.focus()
+  // }, [isRenaming])
+
   const toggleShowDeleteConfirmation = () => setShowDeleteConfirmation(prev => !prev)
+
+  const rename = (value: string) => {
+    setProjects(prev => {
+      return prev.map(project => {
+        return project.id === id ? { ...project, name: value } : project
+      })
+    })
+  }
+
+  const updateColor = (value: string) => {
+    setProjects(prev => {
+      return prev.map(project => {
+        return project.id === id ? { ...project, color: value } : project
+      })
+    })
+  }
 
   const addProjectToFocusPeriod = () => {
     setFocusPeriods(prev => {
@@ -55,6 +111,10 @@ export const ProjectInList = ({ project }: Props) => {
     })
   }
 
+  if (isRecoloring) {
+    console.log('isrecoloring', isRecoloring)
+  }
+
   return (
     <div>
       <div
@@ -62,33 +122,95 @@ export const ProjectInList = ({ project }: Props) => {
         className="relative flex w-full h-16 rounded-xl items-center justify-center cursor-pointer"
         style={{ backgroundColor: color }}
       >
-        <span className="font-bold select-none pointer-events-none">{name}</span>
-        <div className="absolute top-1 right-2 flex gap-2">
-          <Button variant="link" onClick={toggleEdit} className="p-0">
-            <Edit3Icon className="w-4 h-4" />
-          </Button>
+        {/* <Input */}
+        {/*   ref={inputRef} */}
+        {/*   disabled={!isRenaming} */}
+        {/*   value={name} */}
+        {/*   onBlur={() => toggleIsRenaming(false)} */}
+        {/*   onChange={e => rename(e.currentTarget.value)} */}
+        {/*   className="flex h-auto text-center font-bold text-base bg-transparent border-none active:border-none focus:border-none" */}
+        {/* /> */}
 
-          {!project.isArchived && (
-            <Button variant="link" onClick={addProjectToFocusPeriod} className="p-0">
-              <TargetIcon className="w-4 h-4" />
+        <div className="w-full h-full">
+          {isRenaming ? (
+            <Input
+              ref={inputRef}
+              value={name}
+              onChange={e => rename(e.currentTarget.value)}
+              onBlur={() => toggleIsRenaming(false)}
+              className="inline-flex w-full h-full items-center text-center font-bold text-base text-foreground bg-transparent  border-none focus:border-none"
+            />
+          ) : (
+            <Button
+              className={cn(
+                `inline-flex w-full h-full items-center justify-center font-bold text-base text-foreground bg-transparent hover:bg-transparent disabled:opacity-100 select-none`,
+                isArchived ? 'disabled:cursor-default' : 'disabled:cursor-pointer'
+              )}
+              disabled={isArchived}
+              onClick={addProjectToFocusPeriod}
+            >
+              {name}
             </Button>
           )}
+        </div>
 
-          <Button variant="link" onClick={toggleArchiveProject} className="p-0">
-            {project.isArchived ? (
-              <ArchiveRestoreIcon className="w-4 h-4" />
-            ) : (
-              <ArchiveIcon className="w-4 h-4" />
-            )}
-          </Button>
+        <div className="absolute top-2 right-3 flex items-center gap-2">
+          <DropdownMenu open={showMenu} onOpenChange={setShowMenu} modal={false}>
+            <DropdownMenuTrigger>
+              <EllipsisIcon className="w-6 h-6" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent onCloseAutoFocus={e => e.preventDefault()}>
+              <DropdownMenuItem onSelect={() => toggleIsRenaming(true)}>
+                <Edit3Icon className="mr-2 h-4 w-4" />
+                <span>Rename</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem onSelect={() => openRecoloring()}>
+                <div className="mr-2 w-4 h-4" style={{ backgroundColor: color }} />
+                <span>Change Color</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem onSelect={toggleArchiveProject}>
+                {isArchived ? (
+                  <ArchiveRestoreIcon className="mr-2 w-4 h-4" />
+                ) : (
+                  <ArchiveIcon className="mr-2 w-4 h-4" />
+                )}
 
-          <Button variant="link" onClick={toggleShowDeleteConfirmation} className="p-0">
-            <Trash2Icon className="w-4 h-4" />
-          </Button>
+                <span>{isArchived ? 'Unarchive' : 'Archive'} </span>
+              </DropdownMenuItem>
+
+              <DropdownMenuItem onSelect={toggleShowDeleteConfirmation}>
+                <Trash2Icon className="mr-2 h-4 w-4" />
+                <span>Delete</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          {/* <input */}
+          {/*   type="color" */}
+          {/*   value={color} */}
+          {/*   onChange={e => updateColor(e.target.value)} */}
+          {/*   className="min-w-1 w-4 min-h-1 h-4" */}
+          {/* /> */}
         </div>
       </div>
 
-      {editing && <EditProject project={project} />}
+      <Popover open={isRecoloring} onOpenChange={setIsRecoloring}>
+        <PopoverAnchor />
+        <PopoverContent
+          onOpenAutoFocus={e => e.preventDefault()}
+          className="w-full p-0 bg-transparent border-none"
+        >
+          <ColorPicker
+            theme={themes.dark}
+            color={color}
+            onChange={color => updateColor(color.hex)}
+            hideAlpha
+            presets={colors}
+          />
+        </PopoverContent>
+      </Popover>
+
+      {/* {editing && <EditProject project={project} />} */}
 
       {showDeleteConfirmation && (
         <>
